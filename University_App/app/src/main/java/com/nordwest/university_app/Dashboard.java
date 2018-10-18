7b79b29e71e63ea718d5494ce14b7dc2953d743a
NordWest1 <mihairmcr7@gmail.com>
2018-10-18 21:40:35 +0100

    Dashboard done

--- University_App/app/src/main/java/com/nordwest/university_app/Dashboard.java
+++ University_App/app/src/main/java/com/nordwest/university_app/Dashboard.java
@@ -2,15 +2,18 @@ package com.nordwest.university_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class Dashboard extends AppCompatActivity {
+    ImageView timeTable_Card;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
         Toast.makeText(getBaseContext(),"Welcome to the Dashobard",Toast.LENGTH_LONG).show();
     }
 }
