5d7a206653fe92774b9a6c62a01775e451b133de
NordWest01 <mihairmcr7@gmail.com>
2018-11-05 04:14:19 +0000

    registration modified

--- University_App/app/src/main/java/com/nordwest/university_app/register.java
+++ University_App/app/src/main/java/com/nordwest/university_app/register.java
@@ -18,7 +18,7 @@ public class register extends AppCompatActivity {
     SQLiteOpenHelper openHelper;
     SQLiteDatabase db;
     Button _btn_register;
-    EditText _txtFname_, _txtSname_, _txtAddress_, _txtPass_, _txtEmail_;
+    EditText _txtStudID_,_txtFname_, _txtSname_, _txtGroup_, _txtPass_, _txtEmail_;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +26,11 @@ public class register extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         openHelper = new DatabaseHelper(this);
         _btn_register = findViewById(R.id.btn_reg_regAct);
+        _txtStudID_ = findViewById(R.id.reg_Stud_id);
         _txtFname_ = findViewById(R.id.reg_Fname);
         _txtSname_ = findViewById(R.id.reg_Sname);
         _txtEmail_ = findViewById(R.id.reg_Email);
-        _txtAddress_ = findViewById(R.id.reg_Address);
+        _txtGroup_ = findViewById(R.id.reg_Group);
         _txtPass_ = findViewById(R.id.reg_Pass);
 
         _btn_register.setOnClickListener(new View.OnClickListener() {
@@ -37,36 +38,30 @@ public class register extends AppCompatActivity {
             public void onClick(View v) {
 
                 db = openHelper.getWritableDatabase();
-                String Fname = _txtFname_.getText().toString();
-                String Sname = _txtSname_.getText().toString();
-                String Email = _txtEmail_.getText().toString();
-                String Address = _txtAddress_.getText().toString();
-                String Password = _txtPass_.getText().toString();
+                String studID = _txtStudID_.getText().toString();
+                String fName = _txtFname_.getText().toString();
+                String sName = _txtSname_.getText().toString();
+                String email = _txtEmail_.getText().toString();
+                String group = _txtGroup_.getText().toString();
+                String password = _txtPass_.getText().toString();
 
                 ContentValues contentValues = new ContentValues();
-                contentValues.put(Contract.StudentEntry.STUDENT_FNAME,Fname);
-                contentValues.put(Contract.StudentEntry.STUDENT_SNAME,Sname);
-                contentValues.put(Contract.StudentEntry.STUDENT_EMAIL,Email);
-                contentValues.put(Contract.StudentEntry.STUDENT_ADDRESS,Address);
-                contentValues.put(Contract.StudentEntry.STUDENT_PASWD,Password);
+                contentValues.put(Contract.StudentEntry.STUDENT_ID,studID);
+                contentValues.put(Contract.StudentEntry.STUDENT_FNAME,fName);
+                contentValues.put(Contract.StudentEntry.STUDENT_SNAME,sName);
+                contentValues.put(Contract.StudentEntry.STUDENT_EMAIL,email);
+                contentValues.put(Contract.StudentEntry.STUDENT_GROUP,group);
+                contentValues.put(Contract.StudentEntry.STUDENT_PASWD,password);
                 db.insert(Contract.StudentEntry.TABLE_NAME, null, contentValues);
                 db.close();
                 Intent intent = new Intent(register.this,Login.class);
                 startActivity(intent);
 
                  Log.d("Database Operations","New record inserted in table "+ Contract.StudentEntry.TABLE_NAME);
-                Toast.makeText(getApplicationContext(), "register Succesfully", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Registered Successfully", Toast.LENGTH_LONG).show();
             }
 
 
         });
-
-
-
-
     }
-
-
-
-
 }
