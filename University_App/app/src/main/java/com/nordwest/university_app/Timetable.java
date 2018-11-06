55a3a4b14e97d1b5f2cbce7b91f718a8ac3671fc
Mihail Tudos <mihailtudos@outlook.com>
2018-11-06 16:17:45 +0000

    timetable webView done

--- University_App/app/src/main/java/com/nordwest/university_app/Timetable.java
+++ University_App/app/src/main/java/com/nordwest/university_app/Timetable.java
@@ -3,31 +3,57 @@ package com.nordwest.university_app;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Timetable extends ListActivity implements AdapterView.OnItemClickListener {
+public class Timetable extends AppCompatActivity  /* ListActivity implements AdapterView.OnItemClickListener*/ {
 
+/*
     String [] week_day;
+*/
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_timetable);
+        mWebView = new WebView (this);
+        setContentView(mWebView);
+        mWebView.setWebViewClient(new WebViewSampleClient());
+        mWebView.loadUrl("https://myqahe.qabusinessschool.com/Account/StudentTimetable.aspx");
+        mWebView.getSettings().setJavaScriptEnabled(true);
 
-        week_day = getResources().getStringArray(R.array.week_days);
+        /*week_day = getResources().getStringArray(R.array.week_days);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.week_day_list, week_day);
         getListView().setAdapter(arrayAdapter);
 
         getListView().setOnItemClickListener(this);
-        Toast.makeText(getBaseContext(),"Timetable",Toast.LENGTH_LONG).show();
+        Toast.makeText(getBaseContext(),"Timetable",Toast.LENGTH_LONG).show();*/
 
     }
+    public boolean onKeyDown (int keyCode, KeyEvent event){
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()){
+            mWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 
-    @Override
+    private class WebViewSampleClient extends WebViewClient{
+        public boolean shouldOverrideUrlLoading(WebView view, String url){
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+   /* @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         Toast.makeText(this, ((TextView) view).getText(), Toast.LENGTH_LONG).show();
-    }
+    }*/
 }
